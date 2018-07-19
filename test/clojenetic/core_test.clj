e6da524dc1e71aedd333f26badef34ada903f1a2
Emily A Grasmeder <egrasmed@thoughtworks.com>
2018-07-19 12:03:40 +0200

    Try an untested base case

--- test/clojenetic/core_test.clj
+++ test/clojenetic/core_test.clj
@@ -1,7 +1,9 @@
 (ns clojenetic.core-test
   (:require [clojure.test :refer :all]
-            [clojenetic.core :refer :all]))
+            [clojenetic.core :as core]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest base-test
+  (testing "constructs a component with terminals, numbers, and functions"
+    (is (= {:terminals 1
+            :numbers 2
+            :functions 3} (core/base-init 1 2 3)))))
