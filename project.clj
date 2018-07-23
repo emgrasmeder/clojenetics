cb77517b4efcb10581e11577d09c637af7eac271
Emily A Grasmeder <egrasmed@thoughtworks.com>
2018-07-23 20:48:53 +0200

    Add logging

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [circleci/bond "0.3.1"]]
+                 [circleci/bond "0.3.1"]
+                 [org.clojure/tools.logging "0.4.1"]]
   :main ^:skip-aot clojenetics.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/clojenetics/core.clj
+++ src/clojenetics/core.clj
@@ -1,28 +1,29 @@
-(ns clojenetics.core)
+(ns clojenetics.core
+  (:require [clojure.tools.logging :as log]))
 
 (defn set-terminals [state terminals]
-  (println "setting terminals: " terminals)
+  (log/debug "Setting terminals: " terminals)
   (assoc state :terminals terminals))
 
 (defn set-numbers [state numbers]
-  (println "setting numbers: " numbers)
+  (log/debug "Setting numbers: " numbers)
   (assoc state :numbers numbers))
 
 (defn set-functions [state functions]
-  (println "setting functions: " functions)
+  (log/debug "Setting functions: " functions)
   (assoc state :functions functions))
 
 (defn set-objective-fn [state fn]
-  (println "setting objective-fn: " fn)
+  (log/debug "Setting objective-fn: " fn)
   (assoc state :objective-fn fn))
 
 (defn set-tree-depth [state limit]
-  (println "setting tree-depth: " limit)
+  (log/debug "Setting tree-depth: " limit)
   (assoc state :tree-depth limit))
 
 (defn rand-terminal
   [terminals numbers]
-  (println "choosing random terminal")
+  (log/debug "Choosing random terminal")
   (rand-nth (concat terminals numbers)))
 
 (defn try-for-terminal [{:keys [tree-depth terminals numbers]}]
@@ -32,16 +33,13 @@
 
 (defn create-tree
   [{:keys [tree-depth terminals numbers functions] :as state}]
-  #_(println "Creating tree from..." state)
   (or (try-for-terminal state)
       (let [[func arity] (rand-nth functions)
             state (set-tree-depth state (dec tree-depth))]
-        (println "Recursing tree creation with state: " state)
-        (println "func, arity" func arity)
+        (log/debugf "Recursing tree creation with state: %s" state)
         (cons func (repeatedly arity #(create-tree state))))))
 
 (defn solve-objective-fn [state min-or-max]
-  (println "solving objective function with " min-or-max)
   (let [tree (create-tree state)]
     (if (= 0 ((:objective-fn state) tree))
       tree
