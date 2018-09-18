20a34aa1787a52e878a90539b7e43cb8ad915374
Em Grasmeder <emma@gras.org>
2018-09-18 18:02:40 +0200

    Change ns for proper import

--- src/clojenetics/core.clj
+++ src/clojenetics/core.clj
@@ -1,7 +1,7 @@
 (ns clojenetics.core
   (:require [clojure.tools.logging :as log]
-            [clojenetics.logics.trees :as trees]))
+            [clojenetics.logics.generations :as generations]))
 
 (defn grow [state]
   (log/info "Planting trees... " state)
-  (trees/do-generation state))
+  (generations/do-generation state))
